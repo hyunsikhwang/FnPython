4da3beda15d09afc351d6335952080c1d58b05c1
hyunsikhwang <gomting@naver.com>
2017-02-08 23:43:32 +0900

    종목코드없는경우 처리

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ def FindCodeAPI(APIKey, stock_name):
     for li in soup.findAll('item'):
         i = i + 1
         retlist1.append([li.korsecnnm.string])
-        if li.shotnisin == None:
-            li.shotnisin = ''
+        if li.shotnisin.string == None:
+            li.shotnisin.string = ''
         retlist2.append([li.shotnisin.string])
 
     retlist = [retlist1, retlist2]
