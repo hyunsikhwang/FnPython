420c247009f8cb6082a5d227dfd27136ab8c0310
hyunsikhwang <gomting@naver.com>
2017-02-09 14:23:23 +0900

    seibro 종목명 조회 1페이지당 200개에서 500개로 수정 200개 초과하는 키워드 있음("한국")

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ class CommandStatus(ndb.Model):
 
 def FindCodeAPI(APIKey, stock_name):
     url = 'http://api.seibro.or.kr/openapi/service/StockSvc/getStkIsinByNm'
-    queryParams = '?' + urlencode({ quote_plus('ServiceKey') : APIKey, quote_plus('secnNm') : stock_name.encode('utf-8'), quote_plus('pageNo') : '1', quote_plus(u'numOfRows') : '200' })
+    queryParams = '?' + urlencode({ quote_plus('ServiceKey') : APIKey, quote_plus('secnNm') : stock_name.encode('utf-8'), quote_plus('pageNo') : '1', quote_plus(u'numOfRows') : '500' })
 
     request = Request(url + queryParams)
     request.get_method = lambda: 'GET'
