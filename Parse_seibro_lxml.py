1e9cc6cbbb0115f1126d3d2c869efdeb86a0d78a
hyunsikhwang <gomting@naver.com>
2017-02-11 23:50:40 +0900

    종목명 검색 with lxml
    다시 완전 수정

--- Parse_seibro_lxml.py
+++ Parse_seibro_lxml.py
@@ -2,6 +2,9 @@
 from lxml import etree
 import urllib2
 from StringIO import StringIO
+import timeit
+from bs4 import BeautifulSoup
+
 
 url = 'http://api.seibro.or.kr/openapi/service/StockSvc/getStkIsinByNm?ServiceKey=CJL9jdtz5gsb4z4PpjFpCDjdz/UIk8cFAGgHbJvgLEJxPWLZaTx3wIcBNPkGu/KIKsI1zAy1XtfQJLG0VV0vVg==&secnNm=' + u'한국'.encode('euc-kr') + '&pageNo=1&numOfRows=500'
 
@@ -13,9 +16,30 @@ xml = f.read().decode('utf-8')
 f.close()
 
 context = etree.fromstring(xml.encode('utf-8'))
-r = context.xpath('//korSecnNm//text()')
+#r = context.xpath('//item/korSecnNm/text()')
+i = 0
+#temp = etree.XML(xml.encode('utf-8'))
 
+retlist = []
+retlist1 = []
+retlist2 = []
+
+start = timeit.default_timer()
+
+srch = etree.XPath('//item')
+
+
+r = context.xpath('//item/korSecnNm/text()')
+i = 0
+count = 0
 
 for elem in r:
-    print elem
-    i=i+1
+    #print "Number: ", i
+    #print "Item count:", len(srch(temp)[i])
+
+    if len(srch(temp)[i]) == 7:
+        retlist1.append(srch(temp)[i][4].text)
+        retlist2.append(srch(temp)[i][6].text)
+        print retlist1[count], retlist2[count]
+        count = count + 1
+    i = i + 1
