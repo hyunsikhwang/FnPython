52ccb5b55ced613b458f431f05e377946f98090f
hyunsikhwang <gomting@naver.com>
2017-02-19 18:05:37 +0900

    parse_fnguide2 SPS 내용 parse test
    여러 내용 중 일단 SPS 에 대한 parse test 코드 수정

--- parse_fnguide 2.py
+++ parse_fnguide 2.py
@@ -1,13 +1,14 @@
 import lxml.html
 
-url = 'http://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A026960&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=&strResearchYN='
+#url = 'http://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A026960&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=&strResearchYN='
+url = 'http://comp.fnguide.com/SVO2/ASP/SVD_Invest.asp?pGB=1&gicode=A026960&cID=&MenuYn=Y&ReportGB=&NewMenuID=105&stkGb=701'
 
 html = lxml.html.parse(url)
 # packages = html.xpath('//tr/td/a/text()') # get the text inside all "<tr><td><a ...>text</a></td></tr>"
 # packages = html.xpath('//table[@class="gTable clr"]/tr/td/span/text()') # get the text inside all "<tr><td><a ...>text</a></td></tr>"
 # print packages
 
-packages = html.xpath('//div[@class="corp_group2"]/dl/dd/text()')
+packages = html.xpath('//tr[@id="p_grid1_4"]/td/text()')
 # test = html.xpath("//tr/td/a/@href")
 
 A = {}
