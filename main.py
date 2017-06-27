b9478ad5d74a73414fabf2a9096df235113b4809
hyunsikhwang <gomting@naver.com>
2017-06-27 23:45:10 +0900

    공시 리스트 출력#9

--- main.py
+++ main.py
@@ -354,10 +354,12 @@ def cmd_refresh(chat_id):
     send_msg(chat_id, s)
     i = 0
     for i, el in DARTInfo['list']:
-        if DARTInfo['list'][i]['crp_cls'] == "K" or DARTInfo['list'][i]['crp_cls'] == "Y":
-            s = u'%03d 분류 : %s\n' + u'종목명 : %s\n'+ u'종목코드 : %s\n'+ u'보고서명 : %s\n' % \
-                (i, DARTInfo['list'][i]['crp_cls'], DARTInfo['list'][i]['crp_nm'], DARTInfo['list'][i]['crp_cd'], DARTInfo['list'][i]['rpt_nm'])
-            send_msg(chat_id, s)
+#        if DARTInfo['list'][i]['crp_cls'] == "K" or DARTInfo['list'][i]['crp_cls'] == "Y":
+#            s = u'%03d 분류 : %s\n' + u'종목명 : %s\n'+ u'종목코드 : %s\n'+ u'보고서명 : %s\n' % \
+#                (i, DARTInfo['list'][i]['crp_cls'], DARTInfo['list'][i]['crp_nm'], DARTInfo['list'][i]['crp_cd'], DARTInfo['list'][i]['rpt_nm'])
+#            send_msg(chat_id, s)
+        s = "%03d :" % (i) + DARTInfo['list'][i]['crp_nm']
+        send_msg(chat_id, s)
 
     dateint = now.tm_year * 10000 + now.tm_mon * 100 + now.tm_mday
     countint = DARTInfo['total_count']
